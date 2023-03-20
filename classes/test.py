4e287470a09aa21b5b447b8d264d5684de8617fc
anthony <anthony@stackinsat.com>
2023-03-20 17:01:16 +0000

    Classic map function done Next : Add interactive labels and optimize code

--- classes/test.py
+++
@@ -1,9 +0,0 @@
-from classes import transportnetwork as tn
-import networkx as nx
-from preprocessing import Preprocessing as pp
-
-# Create a new transport network
-G = pp.create_network_from_trailway("../data/Railway Data_JL.xlsx")
-TN = tn.TransportNetwork(G, pos_argument=["lon", "lat"], time_arguments=["dep_time", "arr_time"])
-print(TN)
-print(TN.get_max_time(), TN.get_min_time())
