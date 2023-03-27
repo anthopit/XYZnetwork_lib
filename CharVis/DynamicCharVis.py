c4c72c753bd2c41cf9da88546cce71d267fb9e21
anthony <anthony@stackinsat.com>
2023-03-27 16:53:23 +0100

    Added nodes to dynamic map function

--- CharVis/DynamicCharVis.py
+++ CharVis/DynamicCharVis.py
@@ -29,7 +29,7 @@ def get_gradient_color(value):
     cmap_name (str): The name of the Matplotlib colormap to use.
 
     Returns:
-    tuple: A tuple representing the RGB values of the color at the given value on the gradient.
+    tuple: A tuple represG.edges((1351, 692))enting the RGB values of the color at the given value on the gradient.
     """
     cmap = plt.get_cmap('viridis')
     norm = plt.Normalize(vmin=0, vmax=1)
@@ -79,6 +79,43 @@ def map_dynamic_network(TN, spatial=True, generate_html=False, filename="map.htm
     elif TN.is_spatial and spatial:
         pos = TN.pos_dict
 
+    # Define the node trace
+    node_x = []
+    node_y = []
+    for node in TN.graph.nodes():
+        x = pos[node][0]
+        y = pos[node][1]
+        node_x.append(x)
+        node_y.append(y)
+
+    if TN.is_spatial == False or spatial == False:
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                sizemode='area',
+                reversescale=True,
+                color='#888',
+                size=5,
+            )
+        )
+    else:
+        node_trace = go.Scattergeo(
+            lon=node_x, lat=node_y,
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                sizemode='area',
+                reversescale=True,
+                color='#888',
+                size=5,
+            )
+        )
+
+    # Add node trace as the first layer of the figure
+    fig.add_trace(node_trace)
+
     # Add traces, one for each slider step
     for step in np.arange(start, end, step):
         G2 = nx.DiGraph(((source, target, attr) for source, target, attr in TN.multidigraph.edges(data=True) if
@@ -103,14 +140,14 @@ def map_dynamic_network(TN, spatial=True, generate_html=False, filename="map.htm
             edge_trace = go.Scatter(
                 x=lon, y=lat,
                 mode='lines',
-                line=dict(width=1, color='red'),
+                line=dict(width=2, color='red'),
                 opacity=0.5,
             )
         else:
             edge_trace = go.Scattergeo(
                 lon=lon, lat=lat,
                 mode='lines',
-                line=dict(width=1, color='red'),
+                line=dict(width=2, color='red'),
                 opacity=0.5,
             )
 
@@ -118,18 +155,19 @@ def map_dynamic_network(TN, spatial=True, generate_html=False, filename="map.htm
         fig.add_trace(edge_trace)
 
 
-    # Create and add slider
+
     steps = []
     for i in range(len(fig.data)):
         step = dict(
             method="update",
-            args=[{"visible": [False] * len(fig.data)},
+            args=[{"visible": [True] + [False] * len(fig.data)},
                   {"title": "Date time " + str(convert_minutes_to_ddhhmm(i * (end - start) / 100))}],
             # layout attribute
         )
         step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
         steps.append(step)
 
+    # Create and add slider
     sliders = [dict(
         active=10,
         currentvalue={"prefix": "Frequency: "},
@@ -140,50 +178,6 @@ def map_dynamic_network(TN, spatial=True, generate_html=False, filename="map.htm
     fig.update_layout(
         sliders=sliders
     )
-
-    # node_x = []
-    # node_y = []
-    # for node in TN.graph.nodes():
-    #     x = pos[node][0]
-    #     y = pos[node][1]
-    #     node_x.append(x)
-    #     node_y.append(y)
-    #
-    # if node_weigth == True:
-    #     maker_style_dict = dict(
-    #             size=list_node_weigth_scaled ,
-    #             color=list_node_weigth,
-    #             sizemode='area',
-    #             cmin=min(list_node_weigth ),
-    #             cmax=max(list_node_weigth ),
-    #             colorbar_title=TN.nodes_weight_argument,
-    #             reversescale=True,
-    #             line_width=2)
-    # else:
-    #     maker_style_dict = dict(
-    #             sizemode='area',
-    #             reversescale=True,
-    #             color='#888',
-    #             size=5,
-    # )
-    #
-    # if TN.is_spatial == False or spatial == False:
-    #     node_trace = go.Scatter(
-    #             x=node_x, y=node_y,
-    #             mode='markers',
-    #             hoverinfo='text',
-    #             marker=maker_style_dict
-    #     )
-    # else:
-    #     node_trace = go.Scattergeo(
-    #             lon=node_x, lat=node_y,
-    #             mode='markers',
-    #             hoverinfo='text',
-    #             marker=maker_style_dict
-    #     )
-    #
-    # fig.add_trace(node_trace)
-
 
     # Define layout of the map
     fig.update_layout(
@@ -207,128 +201,3 @@ def map_dynamic_network(TN, spatial=True, generate_html=False, filename="map.htm
 
 
     # #TODO : add ineractuive etiquette
-    #
-    # # Define the edges and nodes positions
-    # edge_x = []
-    # edge_y = []
-    # for edge in TN.graph.edges():
-    #     x0 = pos[edge[0]][0]
-    #     y0 = pos[edge[0]][1]
-    #     x1 = pos[edge[1]][0]
-    #     y1 = pos[edge[1]][1]
-    #     edge_x.extend([x0, x1, None])
-    #     edge_y.extend([y0, y1, None])
-    #
-    # node_x = []
-    # node_y = []
-    # for node in TN.graph.nodes():
-    #     x = pos[node][0]
-    #     y = pos[node][1]
-    #     node_x.append(x)
-    #     node_y.append(y)
-    #
-    # if node_weigth == True:
-    #     maker_style_dict = dict(
-    #             size=list_node_weigth_scaled ,
-    #             color=list_node_weigth,
-    #             sizemode='area',
-    #             cmin=min(list_node_weigth ),
-    #             cmax=max(list_node_weigth ),
-    #             colorbar_title=TN.nodes_weight_argument,
-    #             reversescale=True,
-    #             line_width=2)
-    # else:
-    #     maker_style_dict = dict(
-    #             sizemode='area',
-    #             reversescale=True,
-    #             color='#888',
-    #             size=5,
-    #     )
-    #
-    #
-    #
-    # if TN.is_spatial == False or spatial == False:
-    #     if edge_weigth:
-    #         for i, edge in enumerate(TN.graph.edges()):
-    #             fig.add_trace(
-    #                 go.Scatter(
-    #                     x=[pos[edge[0]][0], pos[edge[1]][0]],
-    #                     y=[pos[edge[0]][1], pos[edge[1]][1]],
-    #                     mode='lines',
-    #                     line=dict(width=1, color='rgb' + str(get_gradient_color(list_edge_weigth_scaled[i] / max(list_edge_weigth_scaled)))),
-    #                     opacity=1,
-    #                 )
-    #             )
-    #     else:
-    #         edge_trace = go.Scatter(
-    #             x=edge_x, y=edge_y,
-    #             mode='lines',
-    #             line=dict(width=1, color='#888'),
-    #             opacity=0.5,
-    #         )
-    #
-    #     node_trace = go.Scatter(
-    #         x=node_x, y=node_y,
-    #         mode='markers',
-    #         hoverinfo='text',
-    #         marker=maker_style_dict
-    #     )
-    #
-    #     fig.update_layout(
-    #         showlegend=False,
-    #         hovermode='closest',
-    #         margin=dict(b=20, l=5, r=5, t=40),
-    #         xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-    #         yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-    #         width=1200,
-    #         height=900
-    #     )
-    #
-    # elif TN.is_spatial and spatial:
-    #     if edge_weigth:
-    #         for i, edge in enumerate(TN.graph.edges()):
-    #             fig.add_trace(
-    #                 go.Scattergeo(
-    #                     lon=[pos[edge[0]][0], pos[edge[1]][0]],
-    #                     lat=[pos[edge[0]][1], pos[edge[1]][1]],
-    #                     mode='lines',
-    #                     line=dict(width=1, color='rgb' + str(get_gradient_color(list_edge_weigth_scaled[i] / max(list_edge_weigth_scaled)))),
-    #                     opacity=1,
-    #                 )
-    #             )
-    #     else:
-    #         edge_trace = go.Scattergeo(
-    #             lon=edge_x, lat=edge_y,
-    #             mode='lines',
-    #             line=dict(width=1, color='#888'),
-    #             opacity=0.5,
-    #         )
-    #
-    #     node_trace = go.Scattergeo(
-    #         lon=node_x, lat=node_y,
-    #         mode='markers',
-    #         hoverinfo='text',
-    #         marker=maker_style_dict
-    #     )
-    #     fig.update_layout(
-    #         showlegend=False,
-    #         geo=dict(
-    #             projection_type='azimuthal equal area',
-    #             showland=True,
-    #             landcolor='rgb(243, 243, 243)',
-    #             countrycolor='rgb(204, 204, 204)',
-    #             lataxis=dict(range=[TN.get_min_lat() - 1, TN.get_max_lat() + 1]),
-    #             lonaxis=dict(range=[TN.get_min_lon() - 1, TN.get_max_lon() + 1]),
-    #         ),
-    #         width=1200,
-    #         height=900
-    #     )
-    #
-    # if not edge_weigth:
-    #     fig.add_trace(edge_trace)
-    # fig.add_trace(node_trace)
-    #
-    # if generate_html:
-    #     fig.write_html(filename)
-    #
-    # fig.show()
