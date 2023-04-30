0b806db94678818267e5e1f4337ddeaa41f8955f
Maxime <maxime.chevrier.409@cranfield.ac.uk>
2023-04-30 18:28:16 +0200

    Updated docs, repackaged library, added all imports (except gnn)

--- __init__.py
+++ __init__.py
@@ -6,3 +6,18 @@ Package for manipulating, analysing and plotting/mapping transport networks
 
 from xyznetwork_lib import visualisation, clustering, characterisation, preprocessing, ML, robustness_analysis, classes, \
     GNN
+from visualisation.utils import *
+from visualisation.visualisation import *
+from clustering.cluster import *
+from characterisation.assortativity import *
+from characterisation.path import *
+from characterisation.page_rank import *
+from characterisation.clustering import *
+from characterisation.centrality import *
+from characterisation.distance import *
+from characterisation.degree import *
+from preprocessing.Preprocessing import *
+from ML.embedding import *
+from robustness_analysis.robustness import *
+from robustness_analysis.utils import *
+from classes.transportnetwork import *
